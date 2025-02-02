ebf8afe398174a4eba2ca22a346f8799ea0c8ce6
Max <89996069795max@gmail.com>
2025-02-02 18:23:35 +0300

    add-remove-endpoint

--- src/main/java/ru/yandex/practicum/filmorate/dal/GenreRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/GenreRepository.java
@@ -42,6 +42,7 @@ public class GenreRepository extends BaseRepository<Genre> {
 
     public void addGenres(Integer filmId, List<Integer> genresId) {
         jdbc.batchUpdate(ADD_GENRE_QUERY, new BatchPreparedStatementSetter() {
+            @SuppressWarnings("NullableProblems")
             @Override
             public void setValues(PreparedStatement ps, int i) throws SQLException {
                 ps.setInt(1, filmId);
@@ -61,8 +62,8 @@ public class GenreRepository extends BaseRepository<Genre> {
 
     public Map<Integer, Set<Genre>> getAllByFilms() {
         List<Map<String, Object>> rows = jdbc.queryForList(FIND_ALL_BY_FILMS);
-        return rows.stream().collect(groupingBy(
-                        row -> ((Number) row.get("FILM_ID")).intValue(),
+        return rows.stream().collect(groupingBy(row -> ((Number) row.get("FILM_ID"))
+                        .intValue(),
                         mapping(row -> Genre.builder()
                                 .id((Integer) row.get("GENRE_ID"))
                                 .name((String) row.get("name"))
