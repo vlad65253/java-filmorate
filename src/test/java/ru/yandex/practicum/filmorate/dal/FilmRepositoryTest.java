387baabccea6bdc85be2bf3eed4a2a7dc7492bd9
Kirill <kshef@yandex.ru>
2025-02-07 05:24:53 +0100

    Temporary commit

--- src/test/java/ru/yandex/practicum/filmorate/dal/FilmRepositoryTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/dal/FilmRepositoryTest.java
@@ -1,12 +1,12 @@
 package ru.yandex.practicum.filmorate.dal;
 
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.mapper.*;
 import ru.yandex.practicum.filmorate.model.Director;
@@ -15,6 +15,7 @@ import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Rating;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,16 +23,17 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @JdbcTest
 @AutoConfigureTestDatabase
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @Import({FilmRepository.class,
         FilmRowMapper.class,
         LikesRepository.class})
 class FilmRepositoryTest {
-    private final FilmRepository filmRepository;
     private static Film film1;
     private static Film film2;
     private static Film film3;
-    private final LikesRepository likesRepository;
+    private final FilmRepository filmRepository;
+    private final JdbcTemplate jdbcTemplate;
 
     @BeforeAll
     static void beforeAll() {
@@ -69,6 +71,14 @@ class FilmRepositoryTest {
                 .build();
     }
 
+    @BeforeEach
+    void setUp() {
+        jdbcTemplate.update("INSERT INTO USERS (USER_ID, EMAIL, LOGIN, NAME, BIRTHDAY) VALUES (?, ?, ?, ?, ?)",
+                1, "test1@test.com", "test1", "test1", LocalDate.now());
+        jdbcTemplate.update("INSERT INTO USERS (USER_ID, EMAIL, LOGIN, NAME, BIRTHDAY) VALUES (?, ?, ?, ?, ?)",
+                15, "test15@test.com", "test15", "test15", LocalDate.now());
+    }
+
     @Test
     void getFilms() {
         filmRepository.createFilm(film1);
@@ -81,7 +91,7 @@ class FilmRepositoryTest {
     void getFilmById() {
         filmRepository.createFilm(film1);
         Film film = filmRepository.getFilm(film1.getId());
-        assertThat(film).hasFieldOrPropertyWithValue("id", 13);
+        assertThat(film).hasFieldOrPropertyWithValue("id", 19);
     }
 
     @Test
@@ -94,6 +104,7 @@ class FilmRepositoryTest {
     }
 
     @Test
+    @Order(1)
     void create() {
         filmRepository.createFilm(film1);
         filmRepository.createFilm(film2);
@@ -132,4 +143,66 @@ class FilmRepositoryTest {
         filmRepository.deleteFilm(1);
         assertThrows(NotFoundException.class, () -> filmRepository.getFilm(1));
     }
+
+    @Test
+    void getLikedFilmsByUser() {
+        Set<Integer> likedFilms = filmRepository.getLikedFilmsByUser(1);
+        assertThat(likedFilms).isNotNull();
+    }
+
+    @Test
+    void findFilmsByIds() {
+        filmRepository.createFilm(film1);
+        filmRepository.createFilm(film2);
+
+        Set<Integer> filmIds = new HashSet<>();
+        filmIds.add(film1.getId());
+        filmIds.add(film2.getId());
+
+        Collection<Film> films = filmRepository.findFilmsByIds(filmIds);
+        assertThat(films).isNotEmpty();
+    }
+
+    @Test
+    void getTopFilmsByGenreAndYear_specificGenreAndYear_returnsFilteredFilms() {
+        Film testFilm = filmRepository.createFilm(film1);
+        jdbcTemplate.update("INSERT INTO FILMS_GENRE (FILM_ID, GENRE_ID) VALUES (?, ?)", testFilm.getId(), 1);
+        jdbcTemplate.update("INSERT INTO LIKE_LIST (FILM_ID, USER_ID) VALUES (?, ?)", testFilm.getId(), 1);
+
+        Collection<Film> result = filmRepository.getTopFilmsByGenreAndYear(10, 1, 2022);
+        assertThat(result).isNotEmpty();
+    }
+
+    @Test
+    void getTopFilmsByGenreAndYear_nullGenre_returnsAllYearFilms() {
+        Film testFilm = filmRepository.createFilm(film1);
+        jdbcTemplate.update("INSERT INTO LIKE_LIST (FILM_ID, USER_ID) VALUES (?, ?)",
+                testFilm.getId(), 1);
+
+        Collection<Film> result = filmRepository.getTopFilmsByGenreAndYear(10, null, 2022);
+        assertThat(result).isNotEmpty();
+    }
+
+    @Test
+    void getTopFilmsByGenreAndYear_nullYear_returnsAllGenreFilms() {
+        Film testFilm = filmRepository.createFilm(film1);
+        jdbcTemplate.update("INSERT INTO FILMS_GENRE (FILM_ID, GENRE_ID) VALUES (?, ?)",
+                testFilm.getId(), 1);
+        jdbcTemplate.update("INSERT INTO LIKE_LIST (FILM_ID, USER_ID) VALUES (?, ?)",
+                testFilm.getId(), 1);
+
+        Collection<Film> result = filmRepository.getTopFilmsByGenreAndYear(10, 1, null);
+        assertThat(result).isNotEmpty();
+    }
+
+    @Test
+    void getTopFilmsByGenreAndYear_bothNull_returnsTopFilms() {
+        Film testFilm = filmRepository.createFilm(film1);
+        jdbcTemplate.update("INSERT INTO LIKE_LIST (FILM_ID, USER_ID) VALUES (?, ?)",
+                testFilm.getId(), 1);
+
+        Collection<Film> result = filmRepository.getTopFilmsByGenreAndYear(10, null, null);
+        assertThat(result).isNotEmpty();
+    }
+
 }
