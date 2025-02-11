1d13f459a00b2ffb350f626f56c5ae1e3aa7cf2a
Владислав Г. <vlad65253@gmail.com>
2025-02-11 16:12:24 +0300

    ветка с фиксами проблем

--- src/main/java/ru/yandex/practicum/filmorate/dal/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/FilmRepository.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
