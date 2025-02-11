2e10564453587fa1b76d7612cae017803cd6a8ed
Владислав Г. <vlad65253@gmail.com>
2025-02-11 16:16:21 +0300

    ветка с фиксами проблем

--- src/main/java/ru/yandex/practicum/filmorate/dal/UserRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
